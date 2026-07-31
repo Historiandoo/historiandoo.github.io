67bdb762ff08318e07e571f1f9e19f6800cdab27
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2026-07-31 11:17:41 -0300

    [update] Teste de Melhorias

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -23,23 +23,23 @@ function Home() {
             <div className='row g-4'>
 
                 <div className='col-12 col-sm-6 col-md-4 col-lg-3'>
-                    <div className='card'>
+                    <div className='card h-100'>
                         <img src={caravela} className='card-img-top' alt='Chegada dos portugueses'/>
                         <div className='card-body'>
-                            <h5 className='card-title'> A chegada dos portugueses ao Brasil</h5>
-                            <p className='card-text'> Algum texto </p>
-                             <Link className='btn btn-primary' to="/chegadaPortuguesesBrasil">A chegada dos portugueses ao Brasil</Link>
+                            <h5 className='card-title text center'>A chegada dos portugueses ao Brasil</h5>
+                            <p className='card-text'>  </p>
+                             <Link className='btn btn-primary' to="/chegadaPortuguesesBrasil">Veja mais aqui!</Link>
                         </div>
                     </div>
                 </div>
 
                 <div className='col-12 col-sm-6 col-md-4 col-lg-3'>
-                    <div className='card'>
+                    <div className='card h-100'>
                         <img src={caravela} className='card-img-top' alt='...'/>
                         <div className='card-body'>
-                            <h5 className='card-title'> A chegada dos portugueses ao Brasil</h5>
-                            <p className='card-text'> Algum texto </p>
-                             <Link className='btn btn-primary' to="/chegadaPortuguesesBrasil">A chegada dos portugueses ao Brasil</Link>
+                            <h5 className='card-title'>Decolonialidade</h5>
+                            <p className='card-text'> </p>
+                             <Link className='btn btn-primary' to="/chegadaPortuguesesBrasil">Veja mais aqui!</Link>
                         </div>
                     </div>
                 </div>
